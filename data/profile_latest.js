57fc47ca2cfe5bc1d8888c452c03e13f94ed67f8
Volv3rin <volverin.aka.volvf@gmail.com>
2023-05-11 11:11:55 +0530

    Update profile_latest.js
    trial 1

--- data/profile_latest.js
+++ data/profile_latest.js
@@ -4,7 +4,9 @@ const profileData = {
   sub_title: "IT Infrastructure Manager",
   logoURL: "assets/dp.jpeg",  
   about: { 
-	intro: `As an IT Professional I am having more than 12years of experience in IT Infrastructure Design and Implementation and have worked on various projects in multiple domains which includes IT Software Monitoring, Healthcare, IT services, Telecom sector.`,
+	intro: `As an IT Professional I am having more than 12years of experience in IT Infrastructure Design and Implementation 
+	and have worked on various projects in multiple domains which includes
+	IT Software Monitoring, Healthcare, IT services, Telecom sector.`,
     contact: {
       email: "viveknayee@outlook.com",
       phone: "+91-9727216292",
