b6159334c22378511565bd3d1de4dfddde2798bf
Vivek Nayee <volverin.aka.volvf@gmail.com>
2025-04-08 12:25:55 +0530

    Add files via upload

---
+++ data/vivek_resume.js
@@ -0,0 +1,195 @@
+{
+  "basics": {
+    "name": "Vivek H Nayee",
+    "label": "IT Infrastructure Manager",
+    "image": "assets/dp.jpeg",
+    "email": "viveknayee@outlook.com",
+    "phone": "+91-9727216292",
+    "location": {
+      "address": "Navi Mumbai, IN"
+    },
+    "url": "https://www.linkedin.com/in/vivvekhnayee/",
+    "summary": "As an IT Professional I am having more than 12 years of experience in IT Infrastructure Design and Implementation and have worked on various projects in multiple domains which includes IT Software Monitoring, Healthcare, IT services, Telecom sector."
+  },
+  "work": [
+    {
+      "name": "Jio Platforms Ltd",
+      "position": "Manager - IT Infrastructure Support",
+      "startDate": "2015-10",
+      "endDate": "",
+      "summary": "",
+      "highlights": [
+        "Started working for Jio as a Deputy Manager - IT Infrastructure, I was involved and responsible for Infrastructure design specifications and implementations for in house project of Identity Management",
+        "Successfully implemented Multi Site DR with GSLB (F5 Load balancer)",
+        "Successfully provided solutions w.r.t implementations and server resources for technology shift from OPENAM and Oracle to OPENSOURCE Nodejs and Cassandra Multi DC architecture",
+        "Promoted to Manager (JAN 2019-Present), my profile involved in providing effective solutions for network and infrastructure resource requirements while integrating with in house other business applications and also for cloud [Azure and Google Cloud Platform]",
+        "Project scheduling and tracking with Vendors and different internal support teams. Involved in providing leadership and technical guidance to program management bodies and SMEs"
+      ]
+    },
+    {
+      "name": "Jio Platforms Ltd",
+      "position": "Deputy Manager - IT Infrastructure Support",
+      "startDate": "2015-10",
+      "endDate": "2018-12",
+      "summary": "",
+      "highlights": [
+        "Appointed as a Deputy Manager - IT Infrastructure, I was involved and responsible for Infrastructure design specifications and implementations for in house project of Identity Management",
+        "Successfully implemented Multi Site DR with GSLB (F5 Load balancer)",
+        "Successfully provided solutions w.r.t implementations and server resources for technology shift from OPENAM and Oracle to OPENSOURCE Nodejs and Cassandra Multi DC architecture"
+      ]
+    },
+    {
+      "name": "IGATE Global Solution Ltd",
+      "position": "Senior Systems Engineer",
+      "startDate": "2014-10",
+      "endDate": "2015-09",
+      "summary": "",
+      "highlights": [
+        "As a Senior Systems engineer my profile was to work for multiple AIX/Linux projects.",
+        "Got a good exposure to change management systems, planned firmware upgrades, OS upgrades, Database patching along with mentoring junior team members."
+      ]
+    },
+    {
+      "name": "eClinicalWorks India Pvt Ltd",
+      "position": "Systems Administrator",
+      "startDate": "2013-05",
+      "endDate": "2014-09",
+      "summary": "",
+      "highlights": [
+        "Working as a Systems Administrator, I got experience to manage remote Datacenter operations, Production systems health checks, migration of Datacenters on Hypervisors and in cloud, along with exposure to Firewalls and Network Monitoring and Security aspects",
+        "Successfully implemented Datacenter Monitoring solutions which were useful for Trend Analysis, RCA and various other reports for optimized Datacenter operations.",
+        "Exposure to Virtualization and Clustering with VCenter and VMotion for migration, HA, clustering and management of ESX servers."
+      ]
+    },
+    {
+      "name": "MindArray Systems Pvt Ltd",
+      "position": "System Administrator",
+      "startDate": "2011-05",
+      "endDate": "2013-02",
+      "summary": "",
+      "highlights": [
+        "Start of career by managing the local area network for this firm, learning all the Network and Servers basics, email clients configurations and set up VMware systems on blade servers, installation and configuration of AIX box for the Network performance software implementation (in house product), along with managing testing and Staging environment readiness, performance tuning and health reports via various tools"
+      ]
+    }
+  ],
+  "education": [
+    {
+      "institution": "G.H Patel College of Engineering, Sardar Patel University",
+      "area": "Information Technology",
+      "studyType": "B.E.",
+      "startDate": "2006",
+      "endDate": "2010",
+      "score": "5.4 CGPA"
+    },
+    {
+      "institution": "St Mary\u2019s School, GHSEB Board, Rajkot",
+      "area": "Science",
+      "studyType": "Class XI - XII",
+      "startDate": "2004",
+      "endDate": "2006",
+      "score": "65.40%"
+    },
+    {
+      "institution": "St Mary\u2019s School, GHSEB Board, Rajkot",
+      "area": "General",
+      "studyType": "Class X",
+      "startDate": "1992",
+      "endDate": "2004",
+      "score": "77.57%"
+    }
+  ],
+  "skills": [
+    {
+      "name": "Operating Systems",
+      "keywords": [
+        "RHEL",
+        "Ubuntu",
+        "OpenBSD",
+        "FreeBSD",
+        "Mandriva",
+        "CentOS",
+        "DebianOS",
+        "Solaris 11",
+        "SCO Unixware",
+        "MacOSx Server",
+        "Windows Server \u2013 2003 2008/R2",
+        "VmwareESXi 4&5",
+        "IBM - AIX"
+      ]
+    },
+    {
+      "name": "Tools",
+      "keywords": [
+        "Docker",
+        "Kubernetes",
+        "Ansible",
+        "Terraform",
+        "Jenkins",
+        "Bamboo",
+        "Artifactory",
+        "ElasticSearch",
+        "LogStash",
+        "Kibana",
+        "JBOSS",
+        "Varnish",
+        "EMC Networker",
+        "R1 soft & Symantec CPS",
+        "Zenoss",
+        "Solarwinds/WhatsUp Gold/ThousandEyes Monitoring tools",
+        "Nagios",
+        "LiveVault backup tool",
+        "CHEF utility",
+        "HPOM",
+        "F5",
+        "Nginx",
+        "Azure App Gateway"
+      ]
+    },
+    {
+      "name": "LoadBalancers",
+      "keywords": [
+        "F5",
+        "Nginx",
+        "Azure App Gateway"
+      ]
+    },
+    {
+      "name": "Databases",
+      "keywords": [
+        "MYSQL",
+        "PostgreSQL",
+        "Cassandra"
+      ]
+    }
+  ],
+  "certifications": [
+    {
+      "name": "RedHat Certified Systems Administrator (RHCSA)",
+      "date": "2011-08"
+    },
+    {
+      "name": "RedHat Certified Engineer (RHCE)",
+      "date": "2011-08"
+    },
+    {
+      "name": "VMware Certified Associate (VCA)",
+      "date": "2014-06"
+    }
+  ],
+  "projects": [
+    {
+      "name": "Restaurant Review",
+      "startDate": "2018-06",
+      "endDate": "2018-11",
+      "url": "https://github.com/imvpn22/restaurant-review-pwa",
+      "description": "A web-app to list, view and add reviews of restaurents. The app includes filtering based on location, cuisine and rating. Developed using HTML5, CSS3 and JavaScript with PWA features including responsiveness, offline support and accessibility."
+    },
+    {
+      "name": "WhiteBoard",
+      "startDate": "2017-06",
+      "endDate": "2017-08",
+      "url": "https://github.com/imvpn22/whiteboard",
+      "description": "A simple web-app for collaborative brainstorming sessions. Real-time messaging and sketching platform using Node.js, Express, EJS, CSS, JavaScript, and Socket.io."
+    }
+  ]
+}
